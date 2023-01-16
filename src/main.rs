954da676439dc59b46524012a61f124b401f8cbc
Thomas Bracher <thomas.bracher@doctolib.com>
2023-01-16 19:11:34 +0100

    first "working" attempt

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate core;
 
-use std::collections::{HashMap, VecDeque};
+use std::collections::{HashMap, HashSet, VecDeque};
 
 fn main() {
     println!("Hello, world!");
@@ -44,11 +44,6 @@ struct Relation {
     name: String,
 }
 
-#[derive(PartialEq, Debug)]
-struct Row {
-    value: HashMap<String, Value>,
-}
-
 #[derive(PartialEq, Debug, Clone)]
 struct Property(Proposition);
 
@@ -75,12 +70,56 @@ struct Violation {
     trace: Vec<usize>,
 }
 
-#[derive(PartialEq, Debug, Clone)]
+#[derive(Hash, Eq, PartialEq, Debug, Clone)]
+struct Row {
+    keys: Vec<String>,
+    values: Vec<Value>,
+}
+
+impl Row {
+    pub fn get(&self, key: &String) -> Option<Value> {
+        for (i, k) in self.keys.iter().enumerate() {
+            if k == key {
+                return Some(self.values[i].clone());
+            }
+        }
+        None
+    }
+}
+
+impl Row {
+    fn new() -> Self {
+        Row {
+            keys: vec![],
+            values: vec![],
+        }
+    }
+}
+
+impl<const N: usize> From<[(String, Value); N]> for Row {
+    fn from(arr: [(String, Value); N]) -> Self {
+        let mut res = Row::new();
+        for entry in arr {
+            res.keys.push(entry.0);
+            res.values.push(entry.1);
+        }
+        res
+    }
+}
+
+#[derive(Hash, Eq, PartialEq, Debug, Clone)]
 enum Value {
     Bool(bool),
     Integer(i16),
-    Row(HashMap<String, Value>),
+    Row(Row),
     Set(Vec<Value>),
+}
+
+#[derive(Hash, Eq, PartialEq, Debug, Clone)]
+struct HashableState {
+    pc: Vec<usize>,
+    global: Vec<(String, Value)>,
+    locals: Vec<Vec<(String, Value)>>,
 }
 
 #[derive(PartialEq, Debug, Clone)]
@@ -90,61 +129,99 @@ struct State {
     locals: Vec<HashMap<String, Value>>,
 }
 
-fn eval_prop_local(local: &HashMap<String, Value>, prop: &Proposition) -> Value {
-    match prop {
-        Proposition::LesserEqual(left, right) => {
-            let l = if let Value::Integer(l) = eval_prop_local(local, &left) { l } else { todo!() };
-            let r = if let Value::Integer(r) = eval_prop_local(local, &right) { r } else { todo!() };
-            Value::Bool(l <= r)
+impl State {
+    fn hashable(&self) -> HashableState {
+        HashableState {
+            pc: self.pc.clone(),
+            global: self
+                .global
+                .iter()
+                .map(|(l, r)| (l.clone(), r.clone()))
+                .collect(),
+            locals: self
+                .locals
+                .iter()
+                .map(|l| l.iter().map(|(l, r)| (l.clone(), r.clone())).collect())
+                .collect(),
         }
-        Proposition::Count(set) => {
-            let s = if let Value::Set(s) = eval_prop_local(local, &set) {s} else {todo!()};
-            Value::Integer(i16::try_from(s.len()).expect("set is too large for count"))
-        }
-        Proposition::Var(var) => {
-            local.get(&var.name).expect("oupsy").clone()
-        }
-        Proposition::Literal(value) => {
-            value.clone()
-        }
-        _ => panic!("{:?} is not implemented for evaluation", prop)
-    }
-}
-
-fn eval_prop_row(row: &HashMap<String, Value>, prop: &Proposition) -> Value {
-    match prop {
-        Proposition::Literal(value) => {
-            value.clone()
-        }
-        Proposition::Var(v) => {
-            row.get(&v.name).expect(
-                &format!("couldn't find column '{}' in table...", v.name)
-            ).clone()
-        }
-        _ => panic!("{:?} is not implemented for row evaluation", prop)
     }
 }
 
 fn eval(state: &State, select: &Sigma) -> Value {
     let binding = Value::Set(vec![]);
-    let from = state.global
-        .get(&select.relation.name)
-        .unwrap_or(&binding);
+    let from = state.global.get(&select.relation.name).unwrap_or(&binding);
     match &select.formula {
         Proposition::LesserEqual(left, right) => {
             let mut res = vec![];
-            let rows = if let Value::Set(rows) = from {rows} else { todo!() };
+            let rows = if let Value::Set(rows) = from {
+                rows
+            } else {
+                todo!()
+            };
             for row in rows {
-                let rw = if let Value::Row(rw) = row { rw } else { todo!() };
-                let l = if let Value::Integer(l) = eval_prop_row(rw, &left) { l } else { todo!() };
-                let r = if let Value::Integer(r) = eval_prop_row(rw, &right) { r } else { todo!() };
+                let rw = if let Value::Row(rw) = row {
+                    rw
+                } else {
+                    todo!()
+                };
+                let l = if let Value::Integer(l) = eval_prop_row(rw, &left) {
+                    l
+                } else {
+                    todo!()
+                };
+                let r = if let Value::Integer(r) = eval_prop_row(rw, &right) {
+                    r
+                } else {
+                    todo!()
+                };
                 if l <= r {
                     res.push(row.clone());
                 }
             }
             Value::Set(res)
         }
-        _ => panic!("{:?} not yet implemented", select.formula)
+        _ => panic!("{:?} not yet implemented", select.formula),
+    }
+}
+
+fn eval_prop_local(state: &State, local: &HashMap<String, Value>, prop: &Proposition) -> Value {
+    match prop {
+        Proposition::LesserEqual(left, right) => {
+            let l = if let Value::Integer(l) = eval_prop_local(state, local, &left) {
+                l
+            } else {
+                todo!()
+            };
+            let r = if let Value::Integer(r) = eval_prop_local(state, local, &right) {
+                r
+            } else {
+                todo!()
+            };
+            Value::Bool(l <= r)
+        }
+        Proposition::Count(set) => {
+            let s = if let Value::Set(s) = eval_prop_local(state, local, &set) {
+                s
+            } else {
+                todo!()
+            };
+            Value::Integer(i16::try_from(s.len()).expect("set is too large for count"))
+        }
+        Proposition::Var(var) => local.get(&var.name).expect("oupsy").clone(),
+        Proposition::Literal(value) => value.clone(),
+        Proposition::Sigma(sigma) => eval(state, sigma),
+        _ => panic!("{:?} is not implemented for evaluation", prop),
+    }
+}
+
+fn eval_prop_row(row: &Row, prop: &Proposition) -> Value {
+    match prop {
+        Proposition::Literal(value) => value.clone(),
+        Proposition::Var(v) => row
+            .get(&v.name)
+            .expect(&format!("couldn't find column '{}' in table...", v.name))
+            .clone(),
+        _ => panic!("{:?} is not implemented for row evaluation", prop),
     }
 }
 
@@ -165,7 +242,7 @@ fn apply_statement(state: &State, idx: usize, client: &Client) -> State {
             new_state
         }
         Statement::If(condition, offset) => {
-            let proposition_res = eval_prop_local(&state.locals[idx], condition);
+            let proposition_res = eval_prop_local(&state, &state.locals[idx], condition);
             if proposition_res == Value::Bool(true) {
                 new_state.pc[idx] += 1;
                 new_state
@@ -178,23 +255,58 @@ fn apply_statement(state: &State, idx: usize, client: &Client) -> State {
         }
         Statement::Insert(relation, row) => {
             new_state.pc[idx] += 1;
-            let table = if let Value::Set(table) = new_state.global.entry(relation.name.clone()).or_insert(Value::Set(vec![])) { table } else { todo!() };
-            table.push(Value::Row(row.value.clone()));
+            let table = if let Value::Set(table) = new_state
+                .global
+                .entry(relation.name.clone())
+                .or_insert(Value::Set(vec![]))
+            {
+                table
+            } else {
+                todo!()
+            };
+            table.push(Value::Row(row.clone()));
             new_state.global.iter_mut();
             new_state
         }
     }
 }
 
-fn model_checker(clients: &Vec<Client>, _properties: &Vec<Property>) -> Option<Violation> {
-    let mut deq = VecDeque::from([State {
+fn check_property(state: &State, property: &Property) -> Value {
+    let prop = if let Property(Proposition::Always(prop)) = property {
+        prop
+    } else {
+        todo!()
+    };
+    eval_prop_local(state, &state.global, prop)
+}
+
+fn model_checker(clients: &Vec<Client>, properties: &Vec<Property>) -> Option<Violation> {
+    let init_state = State {
         pc: clients.iter().map(|_| 0).collect(),
         global: HashMap::new(),
         locals: clients.iter().map(|_| HashMap::new()).collect(),
-    }]);
-    let mut state_checked:u128 = 0;
+    };
+
+    let mut deq = VecDeque::from([init_state]);
+    let mut visited = HashSet::new();
+    let mut state_checked: u128 = 0;
+
     while let Some(state) = deq.pop_front() {
         // println!("checking for state: {:?}", state);
+        if visited.contains(&state.hashable()) {
+            continue;
+        }
+        visited.insert(state.hashable());
+
+        for property in properties {
+            if check_property(&state, property) != Value::Bool(true) {
+                return Some(Violation {
+                    property: property.clone(),
+                    trace: state.pc,
+                });
+            }
+        }
+
         state_checked += 1;
         for (idx, client) in clients.iter().enumerate() {
             if state.pc[idx] < client.statements.len() {
@@ -209,8 +321,7 @@ fn model_checker(clients: &Vec<Client>, _properties: &Vec<Property>) -> Option<V
 
 #[cfg(test)]
 mod test {
-    use std::collections::HashMap;
-    use crate::{IsolationLevel, model_checker, Client, Property, Proposition, Relation, Sigma, Statement, Variable, Violation, Row, Value};
+    use crate::*;
 
     #[test]
     fn run_process_with_correct_isolation_level() {
@@ -237,48 +348,52 @@ mod test {
                         },
                     ),
                     Statement::If(
-                        Proposition::LesserEqual(Box::new(Proposition::Count(Box::new(Proposition::Var(Variable {
-                            name: "under_18".to_string(),
-                        })))), Box::new(Proposition::Literal(Value::Integer(1)))), 1),
+                        Proposition::LesserEqual(
+                            Box::new(Proposition::Count(Box::new(Proposition::Var(Variable {
+                                name: "under_18".to_string(),
+                            })))),
+                            Box::new(Proposition::Literal(Value::Integer(1))),
+                        ),
+                        1,
+                    ),
                     Statement::Insert(
                         Relation {
                             name: "users".to_string(),
                         },
-                        Row {
-                            value: HashMap::from([("age".to_string(), Value::Integer(12))])
-                        },
+                        Row::from([("age".to_string(), Value::Integer(12))]),
                     ),
                     Statement::Commit,
                 ],
             });
         }
 
-        let properties = vec![Property(
-            Proposition::Always(
-                Box::new(Proposition::LesserEqual(
-                    Box::new(Proposition::Count(Box::new(Proposition::Sigma(Box::new(
-                        Sigma {
-                            formula: Proposition::LesserEqual(
-                                Box::new(Proposition::Var(Variable {
-                                    name: "age".to_string(),
-                                })),
-                                Box::new(Proposition::Literal(Value::Integer(18))),
-                            ),
-                            relation: Relation {
-                                name: "users".to_string(),
-                            },
+        let properties = vec![Property(Proposition::Always(Box::new(
+            Proposition::LesserEqual(
+                Box::new(Proposition::Count(Box::new(Proposition::Sigma(Box::new(
+                    Sigma {
+                        formula: Proposition::LesserEqual(
+                            Box::new(Proposition::Var(Variable {
+                                name: "age".to_string(),
+                            })),
+                            Box::new(Proposition::Literal(Value::Integer(18))),
+                        ),
+                        relation: Relation {
+                            name: "users".to_string(),
                         },
-                    ))))),
-                    Box::new(Proposition::Literal(Value::Integer(1))),
-                ),
-                )
-            ))];
+                    },
+                ))))),
+                Box::new(Proposition::Literal(Value::Integer(1))),
+            ),
+        )))];
 
         let counter_example = model_checker(&clients, &properties);
 
-        assert_eq!(counter_example, Some(Violation {
-            property: properties[0].clone(),
-            trace: vec![0, 1, 2, 0, 1, 2, 0, 1, 2],
-        }))
+        assert_eq!(
+            counter_example,
+            Some(Violation {
+                property: properties[0].clone(),
+                trace: vec![0, 1, 2, 0, 1, 2, 0, 1, 2],
+            })
+        )
     }
 }
